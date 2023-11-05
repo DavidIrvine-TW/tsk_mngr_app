8880fd1c66bd4d9d6069bfa2f88582e6b47f3ccf
David Irvine <109934851+DavidIrvine-TW@users.noreply.github.com>
2023-11-06 06:56:00 +0700

    added a button dispatch to mobile (emptyboard)

--- src/components/emptyboard/EmptyBoard.jsx
+++ src/components/emptyboard/EmptyBoard.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import AddIcon from "@mui/icons-material/Add";
+import {modalIsOpen} from '../../redux/modalSlice'
+import { useDispatch } from 'react-redux';
 
-const EmptyBoard = ({setBoardMenu, setBoardMode, setCreateBoardMenu}) => {
+const EmptyBoard = () => {
+  const dispatch = useDispatch()
   return (
     <section className='w-full flex items-start '>
       <article className='flex flex-col text-center' >
         <p>There are no boards. You must create a new board to get things started...</p>
 
         <button
-          // onClick={() => {
-          //   setBoardMenu(false)
-          //   setBoardMode('add')
-          //   setCreateBoardMenu(true)}}
+          onClick={() => {
+            dispatch(modalIsOpen({type: "createBoard"}))}}
           className="text-md mt-[2rem] tb:hidden"
         >
           <AddIcon sx={{ fontSize: ".75rem" }} />
